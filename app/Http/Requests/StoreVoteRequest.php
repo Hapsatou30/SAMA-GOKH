99eabeda03818444a2e436fde8ca1ec7abb3f44f
loctetgenius <sagnamoussa546@gmail.com>
2024-08-08 17:27:04 +0000

    Ajout de la fonctionnalité de vote pour un projet

--- app/Http/Requests/StoreVoteRequest.php
+++ app/Http/Requests/StoreVoteRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreVoteRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class StoreVoteRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,17 @@ class StoreVoteRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'projet_id' => ['required', 'exists:projets,id'],
+            'habitant_id' => ['required', 'exists:habitants,id'],
+            'statut' => ['required', 'in:pour,contre'],
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(
+            ['success' => false, 'errors' => $validator->errors()],
+            422
+        ));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VoteController;
 use App\Http\Controllers\ProjetController;
 
 Route::get('/user', function (Request $request) {
@@ -11,3 +12,7 @@ Route::get('/user', function (Request $request) {
 // Route pour la modification et la suppression de projet.
 Route::apiResource('projets', ProjetController::class)->only('destroy');
 Route::post('projets/{projet}', [ProjetController::class, 'update']);
+
+// Route pour vote
+Route::apiResource('votes', VoteController::class)->only('store');
+
