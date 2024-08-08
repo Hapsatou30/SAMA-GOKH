c7575cb3ff7bb20d6ca1057086705d163c9a002c
Haps <thiamhapstou@gmail.com>
2024-08-08 16:19:40 +0000

    ajout de la fonctionnalité de déconnexion

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -17,47 +17,61 @@ class ApiController extends Controller
             'password' => ['required', 'string', 'min:8'],
             'role_id' => ['required', 'exists:roles,id'], // Valider que le rôle existe
         ]);
-    
+
         // Enregistrement des données
         $user = User::create([
             'email' => $request->email,
             'password' => bcrypt($request->password),
             'role_id' => $request->role_id, // Assigner le rôle à l'utilisateur
         ]);
-    
+
         // Envoi du token d'authentification
         $token = $user->createToken('authToken')->plainTextToken;
-    
+
         return response()->json([
             "status" => true,
             "message" => "Utilisateur enregistré avec succès",
             "token" => $token,
         ]);
     }
-    
+
     // connexion
     public function login(Request $request)
     {
-       $validator = validator(
-         $request->all(),
-         [
-           'email' =>'required|email|string',
-           'password' => 'required|string',
-         ]
-         );
-         if ($validator->fails()) {
-           return response()->json(['error' => $validator->errors()], 422);
-         }
-         $credentials = $request->only('email', 'password');
-            $token = auth()->attempt($credentials);
-            if (!$token) {
-               return response()->json(['message' => 'Information de connexion incorrectes'], 401);
-            }
-            return response()->json([
-                "access_token" => $token,
-                "token_type" => "bearer",
-                "user" => auth()->user(), 
-                "expires_in" => env ("JwT_TTL") * 30  . 'seconds'
-            ]);
-}
+        // Validation des données
+        $validator = validator(
+            $request->all(),
+            [
+                'email' => 'required|email|string',
+                'password' => 'required|string',
+            ]
+        );
+        // Si les données ne sont pas valides, renvoyer les erreurs
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+        // Si les données sont valides, authentifier l'utilisateur
+        $credentials = $request->only('email', 'password');
+        $token = auth()->attempt($credentials);
+        // Si les informations de connexion ne sont pas correctes, renvoyer une erreur 401  
+        if (!$token) {
+            return response()->json(['message' => 'Information de connexion incorrectes'], 401);
+        }
+        // Renvoyer le token d'authentification
+        return response()->json([
+            "access_token" => $token,
+            "token_type" => "bearer",
+            "user" => auth()->user(),
+            "expires_in" => env("JwT_TTL") * 30  . 'seconds'
+        ]);
+    }
+
+    // déconnexion
+    public function logout(Request $request){
+        auth()->logout();
+        return response()->json([
+            'status' =>true,
+           'message' => 'Utilisateur déconnecté'
+        ]);
+    }
 }
