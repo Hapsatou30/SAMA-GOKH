89a4a2b92b2e7085ea338b98d95c7469448575d1
Haps <thiamhapstou@gmail.com>
2024-08-09 09:01:17 +0000

    mis a jour du develop

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolesPermissionsTableSeeder::class,
-            ProjetSeeder::class,
-            HabitantSeeder::class
+          
+            
         ]);
 
     }
